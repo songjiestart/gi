c33029bc0749fd2607795c57d1dfc10ae633d947
songjie <zhaisongjie@benmu_health.com>
2019-04-29 10:26:53 +0800

    dsfadf

---
+++ src/main/java/com/songjie/benmu/songjie/controller/WastController.java
@@ -0,0 +1,12 @@
+package com.songjie.benmu.songjie.controller;
+
+/**
+ * @Author: songjie
+ * @Date: 2019-04-29 10:26
+ * @Description:
+ */
+public class WastController {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
